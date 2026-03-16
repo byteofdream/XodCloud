36bb604bd0e08d9aaf78bd73eec5f29a71e4df3c
DreamyByte Labs <dozirod@mail.ru>
2026-03-16 22:27:08 +0200

    Create footer.php

---
+++ app/views/partials/footer.php
@@ -0,0 +1,15 @@
+</main>
+<div class="mini-player" id="mini-player">
+    <div class="mini-track">
+        <span class="mini-title">Nothing playing</span>
+        <span class="mini-artist">-</span>
+    </div>
+    <button class="mini-play" id="mini-play">Play</button>
+</div>
+<footer class="site-footer">
+    <div>XodCloud · Built for demo purposes · Inspired by SoundCloud</div>
+</footer>
+<script src="https://unpkg.com/wavesurfer.js@7"></script>
+<script src="/assets/js/app.js"></script>
+</body>
+</html>
