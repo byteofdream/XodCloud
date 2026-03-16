1c0b6d34734c2a3dcf7489833ee153c238c1ef9f
DreamyByte Labs <dozirod@mail.ru>
2026-03-16 22:28:09 +0200

    Create Controller.php

---
+++ app/core/Controller.php
@@ -0,0 +1,19 @@
+<?php
+class Controller {
+    protected function view(string $view, array $data = []): void {
+        extract($data);
+        require __DIR__ . '/../views/partials/header.php';
+        require __DIR__ . '/../views/' . $view . '.php';
+        require __DIR__ . '/../views/partials/footer.php';
+    }
+
+    protected function viewPlain(string $view, array $data = []): void {
+        extract($data);
+        require __DIR__ . '/../views/' . $view . '.php';
+    }
+
+    protected function redirect(string $path): void {
+        header('Location: ' . $path);
+        exit;
+    }
+}
