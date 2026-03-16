b544095440bc674ddc32e9cc91feb31e4ecf7b47
DreamyByte Labs <dozirod@mail.ru>
2026-03-16 22:29:03 +0200

    Add files via upload

---
+++ app/models/HomeController.php
@@ -0,0 +1,23 @@
+﻿<?php
+require_once __DIR__ . '/../core/Controller.php';
+require_once __DIR__ . '/../models/Track.php';
+
+class HomeController extends Controller {
+    public function index(): void {
+        $trackModel = new Track();
+        $tracks = $trackModel->all();
+        $this->view('home', ['tracks' => $tracks]);
+    }
+
+    public function stream(): void {
+        $trackModel = new Track();
+        $tracks = $trackModel->all();
+        $this->view('stream', ['tracks' => $tracks]);
+    }
+
+    public function library(): void {
+        $trackModel = new Track();
+        $tracks = $trackModel->all();
+        $this->view('library', ['tracks' => $tracks]);
+    }
+}
